528475ec888c184bea85511399b2f173199799e1
pulorsok <pulorsok@gmail.com>
2018-03-10 17:42:57 +0800

    Add infvovis active button

--- application/views/templates/content/content_head.php
+++ application/views/templates/content/content_head.php
@@ -3,17 +3,19 @@
 
 			<div class="col-lg-8">
 				
-				<ul class="nav nav-tabs">
+				<ul class="nav nav-tabs" id="infovisActive">
 					
 					<li class="active"><a href="#two" data-toggle="tab"> 列表示搜尋</a></li>
-					<li><button class="infovisActive a"><a class="infovisActive" href="#three" data-toggle="tab"> 圖形化搜尋</a></button></li>
+					<li><a class="infovisActive" href="#three" data-toggle="tab"> 圖形化搜尋</a></li>
 							
 				</ul>
 				<div class="tab-content">
 <script type="text/javascript">
 	console.log('fuck');
-	var header = document.getElementById("nav nav-tabs");
-	var btn = header.getElementsByClassName("infovisActive a");
+	var header = document.getElementById("infovisActive");
+	console.log(header);
+	var btn = header.getElementsByClassName("infovisActive");
+	console.log(header);
 	btn[0].addEventListener("click", function(){
 		console.log("active");
 		init(window.json);
